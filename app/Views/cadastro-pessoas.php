b442f37cbfd4ed48f86fabf3d9a3d7059921398b
sprungsumobr <48767935+sprungsumobr@users.noreply.github.com>
2022-05-04 14:29:07 -0300

    asdasd

---
+++ app/Views/cadastro-pessoas.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <div class="col">
+        <h2>Cadastro de Pessoas</h2>
+    </div>
+</div>
+
+<form action="" method="post">
+    <div class="row">
+        <div class="col">
+            <div class="forms-group">
+                <label for="">Nome: </label>
+                <input type="text" name="nome" id="" class="form-control">
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="forms-group">
+                <label for="">Profissão: </label>
+                <input type="text" name="profissao" id="" class="form-control">
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="forms-group">
+                <label for="">Idade: </label>
+                <input type="number" name="idade" id="" class="form-control">
+            </div>
+        </div>
+    </div>
+</form>
